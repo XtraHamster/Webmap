eec0d24e124bb3692430f9b9d8b78b7e1bd64b83
Martin Krecek <martinkrecek98@seznam.cz>
2020-07-02 12:48:01 +0200

    Added layers

---
+++ webmap.py
@@ -0,0 +1,28 @@
+import folium
+import pandas as pd 
+
+data = pd.read_csv('Volcanoes.txt')
+lon = list(data['LON'])
+lat = list(data['LAT'])
+elev = list(data['ELEV'])
+
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000 <= elevation < 2000:
+        return 'orange'
+    else:
+        return 'red' 
+
+map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles='Stamen Terrain')
+
+fg = folium.FeatureGroup(name='My Map')
+
+for lt, ln, el in zip(lat, lon, elev):
+    fg.add_child(folium.CircleMarker(location=[lt, ln], radius = 6, popup=str(el)+'m',
+    fill_color=color_producer(el), color = 'grey', fill_opacity = 0.7))
+
+fg.add_child(folium.GeoJson(data=(open('world.json', 'r', encoding='utf-8-sig').read())))
+
+map.add_child(fg)
+map.save("Map1.html")
